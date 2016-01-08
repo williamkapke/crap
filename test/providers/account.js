54d95fa5ab582e341ffdf8b01127bad6bf59820f
Tom Lee <tom.lee@gotinder.com>
2016-01-07 17:50:00 -0800

    add deps helper function

--- test/providers/account.js
+++ test/providers/account.js
@@ -1,38 +1,27 @@
 var resources = require('../resources.js');
+var crap = require('../../crap');
 
 module.exports = function auto(dependencies, callback){
   console.log("account provider initializing...");
   callback(null, dependencies);
 };
 
-var extend = function(a, b) {
-  for (k in b) {
-    a[k] = b[k];
-  }
-}
-
 // deps usage example.
 // console.log(module.exports.deps({get:1}));
 // console.log(module.exports.deps({getConnections:0}));
 // console.log(module.exports.deps({}));
-module.exports.deps = function(restrict) {
-  restrict = restrict || {};
-  var mapping = {
-    get: {users: resources.users, facebook: resources.facebook},
-    getConnections: {connections: resources.connections}
-  }
-  var included = {};
-  var excluded = {};
-  var useInclude = false;
-  for (k in mapping) {
-    if (restrict[k]) {
-      extend(included, mapping[k]);
-      useInclude = true;
-    } else if (restrict[k] == undefined) {
-      extend(excluded, mapping[k]);
+var mapping = {
+  get: {
+    resources: {
+      users: resources.users,
+      facebook: resources.facebook
+    }
+  },
+  getConnections: {
+    resources: {
+      connections: resources.connections
     }
   }
-  return {
-    resources: (useInclude ? included : excluded)
-  };
-}
+};
+
+module.exports.deps = crap.deps(mapping);
